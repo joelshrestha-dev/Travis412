b4a8c5d9c5f1456d32cd44d1a4d4fecf76e0a1a4
Joel Shrestha <joelshrestha57@gmail.com>
2025-12-07 03:09:54 -0500

    meow

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -1,7 +1,20 @@
 package travis_ci_tutorial_java;
 
 public class SimpleCalculator {
-	public int add(int a, int b) {
-		return a + b;
-	}
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int minus(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;  // assume valid input for now
+    }
 }
--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -11,4 +11,29 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.add(1, 1), 0);
 	}
+	
+	@Test
+	public void testMinus() {
+	    SimpleCalculator calculator = new SimpleCalculator();
+	    assertEquals(2, calculator.minus(5, 3));
+	}
+
+	@Test
+	public void testMultiply() {
+	    SimpleCalculator calculator = new SimpleCalculator();
+	    assertEquals(15, calculator.multiply(5, 3));
+	}
+
+	@Test
+	public void testDivide() {
+	    SimpleCalculator calculator = new SimpleCalculator();
+	    assertEquals(2, calculator.divide(6, 3));
+	}
+	
+	@Test(expected = ArithmeticException.class)
+	public void testDivideByZero() {
+	    SimpleCalculator calculator = new SimpleCalculator();
+	    calculator.divide(10, 0);
+	}
+
 }
